3ed5dab9a83e814226a76614dab6b2a8a466ecfc
TheRealKevBot <Kglas001@gmail.com>
2021-08-13 12:40:44 -0600

    First

---
+++ index.js
@@ -0,0 +1,14 @@
+//  Create a function that takes two numbers and a mathematical operator + - / * 
+//  and will perform a calculation with the given numbers.
+
+function calculator(num1, operator, num2) {
+	
+} 
+
+console.log(calculator(2, '/', 2), 1)
+console.log(calculator(10, '-', 7), 3)
+console.log(calculator(2, '*', 16), 32)
+console.log(calculator(2, '-', 2), 0)
+console.log(calculator(15, '+', 26), 41)
+console.log(calculator(2, '+', 2), 4)
+console.log(calculator(2, "/", 0), "Can't divide by 0!") 
